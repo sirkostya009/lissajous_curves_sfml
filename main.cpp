71a2782bbbcc08390c9a903499aa4561d48f726f
ktovstyk <ktovstyk@igm.technology>
2023-09-16 18:40:36 +0300

    Refactoring and adjustments

--- main.cpp
+++ main.cpp
@@ -9,8 +9,7 @@
 namespace {
 	bool greenOrange{ false };
 
-	constexpr auto  wWIDTH{ 768 };
-	constexpr auto wHEIGHT{ 768 };
+	constexpr auto wWIDTH{ 768 }, wHEIGHT{ 768 };
 
 	constexpr auto PI{ 3.14159265359 }, dPI{ 6.28318530718 };
 	int precision{ 5 }, alpha{ 0 }, beta{ 0 }, scale{ 35 };
@@ -31,13 +30,13 @@ auto makeCurve() {
 			(wWIDTH * (scale / 100.0)) * std::sin(i * alpha) + wWIDTH / 2, wHEIGHT / 2 + std::cos(i * beta) * (wHEIGHT * (scale / 100.0)) // x = sin(i * alpha) y = cos(i * beta)
 		), (greenOrange) ? sf::Color::White : sf::Color::Black));
 
-			std::cout << "i: " << i << "\tx: " << curve[i].position.x << "\ty: " << curve[i].position.y << "\tsin(i): " << std::sin(i) << "\tcos(4i): " << std::cos(i * 4) << '\n';
+		// std::cout << "i: " << i << "\tx: " << curve[i].position.x << "\ty: " << curve[i].position.y << "\tsin(i): " << std::sin(i) << "\tcos(4i): " << std::cos(i * 4) << '\n';
 	}
 
 	return curve;
 }
 
-inline void drawAxes(auto& w) {
+inline void drawAxes(sf::RenderWindow& w) {
 	const static sf::Color col(192, 192, 192);
 
 	static sf::Vertex horizontalLine[]{
@@ -53,7 +52,7 @@ inline void drawAxes(auto& w) {
 	w.draw(verticalLine, 2, sf::LinesStrip);
 }
 
-void drawGreenOrange(auto& w) {
+void drawGreenOrange(sf::RenderWindow& w) {
 	static sf::Vertex box[]{
 		sf::Vertex(sf::Vector2f(0, 0), sf::Color(255, 64, 0)),
 		sf::Vertex(sf::Vector2f(wWIDTH, 0), sf::Color(128, 191, 0)),
@@ -69,8 +68,7 @@ void drawGreenOrange(auto& w) {
 		if (goGreen[i]) {
 			box[i].color.r--;
 			box[i].color.g++;
-		}
-		else {
+		} else {
 			box[i].color.r++;
 			box[i].color.g--;
 		}
@@ -82,19 +80,8 @@ void drawGreenOrange(auto& w) {
 	}
 }
 
-inline void clearIOBuffers() {
-	std::cout << std::endl;
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	std::cin.clear();
-}
-
-int main() {
-	std::vector<sf::Vertex> c;
-	sf::RenderWindow rw(sf::VideoMode(wWIDTH, wHEIGHT), L"Lissajous Curve", sf::Style::Close, sf::ContextSettings(0, 0, 8));
-
-
-
-	auto console = [&]() { // making a lambda for a thread is retarded, i know, but msvc doesnt allow me to use a normal function in std::thread
+inline auto consoleListener(std::vector<sf::Vertex>& c, sf::RenderWindow& rw) {
+	return [&]() {
 		std::string query;
 		std::cout << std::fixed;
 		while (true) {
@@ -105,17 +92,14 @@ int main() {
 				std::cin >> query;
 				if (setMap[query] != nullptr) std::cin >> *setMap[query];
 				else std::cout << "Could not define: " << query;
-			}
-			else if (query == "get") {
+			} else if (query == "get") {
 				std::cin >> query;
 				if (setMap[query] != nullptr) std::cout << query << " = " << *setMap[query];
 				else std::cout << "Could not define: " << query;
-			}
-			else if (query == "exit") {
+			} else if (query == "exit") {
 				rw.close();
 				break;
-			}
-			else if (query == "help") {
+			} else if (query == "help") {
 				std::cout <<
 					"set\t: sets value for a specified variable\n"
 					"get\t: gets value for a specified variable\n"
@@ -123,25 +107,29 @@ int main() {
 					"help\t: shows this text\n"
 					"compile\t: recalculate curve\n"
 					"green-orange\t: a special render";
-			}
-			else if (query == "compile") {
+			} else if (query == "compile") {
 				using clock = std::chrono::steady_clock;
 				auto start = clock::now();
 				c = makeCurve();
 				auto elapsed = clock::now() - start;
-				std::cout << "Compiled in " << std::chrono::duration<double>(elapsed);
-			}
-			else if (query == "green-orange") {
+				std::cout << "Compiled in " << elapsed.count();
+			} else if (query == "green-orange") {
 				greenOrange = !greenOrange;
 				c = makeCurve();
-			}
-			else std::cout << "Could not get query: " << query;
+			} else std::cout << "Could not get query: " << query;
 
-			clearIOBuffers();
+			std::cout << std::endl;
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cin.clear();
 		}
-	}; std::thread t(console);
+	};
+}
 
+int main() {
+	std::vector<sf::Vertex> c;
+	sf::RenderWindow rw(sf::VideoMode(wWIDTH, wHEIGHT), L"Lissajous Curve", sf::Style::Close, sf::ContextSettings(0, 0, 8));
 
+	std::thread t(consoleListener(c, rw));
 
 	rw.setFramerateLimit(30);
 
